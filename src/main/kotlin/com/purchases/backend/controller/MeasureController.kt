c2f13377b44f3ee9018e075f33510ce8c7cd7faf
Andrey Plis <plisandrey91@gmail.com>
2018-06-07 18:53:01 +0300

    updated measure controller

--- src/main/kotlin/com/purchases/backend/controller/MeasureController.kt
+++ src/main/kotlin/com/purchases/backend/controller/MeasureController.kt
@@ -1,17 +1,59 @@
 package com.purchases.backend.controller
 
 import com.purchases.backend.model.Measure
-import com.purchases.backend.repository.MeasureRepository
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import com.purchases.backend.service.MeasureService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
-class MeasureController {
-    @Autowired
-    private val measureRepository: MeasureRepository? = null
+@RequestMapping("/measures")
+class MeasureController(val measureService: MeasureService) {
 
-    val measures: List<Measure>
-        @GetMapping("/measures")
-        get() = measureRepository!!.findAll()
+    @GetMapping
+    @ResponseBody
+    fun retrieveAllGoods(): List<Measure> {
+        return measureService.getAllMeasures()
+    }
+
+    @GetMapping("/{name}")
+    @ResponseBody
+    fun retrieveGood(@PathVariable name: String): ResponseEntity<Any> {
+        val goodOptional = measureService.findMeasure(name)
+        if (goodOptional.isPresent)
+            return ResponseEntity(goodOptional.get(), HttpStatus.OK)
+        else
+            return ResponseEntity.notFound().build()
+    }
+
+
+    @PostMapping
+    fun createGood(@RequestBody measure: Measure): ResponseEntity<Any> {
+        val newGood = measureService.addMeasure(measure)
+
+        val location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{name}")
+                .buildAndExpand(newGood.name).toUri()
+
+        return ResponseEntity.created(location).build()
+
+    }
+
+    @DeleteMapping("/{name}")
+    fun deleteGood(@PathVariable name: String): ResponseEntity<Any> {
+        val goodOptional = measureService.findMeasure(name)
+
+        if (!goodOptional.isPresent)
+            return ResponseEntity.notFound().build()
+
+        measureService.deleteMeasure(goodOptional.get())
+
+        return ResponseEntity.noContent().build()
+    }
+
+    @DeleteMapping
+    fun deleteAllGoods(): ResponseEntity<Any> {
+        measureService.deleteAllMeasures()
+        return ResponseEntity.noContent().build()
+    }
 }
--- src/main/kotlin/com/purchases/backend/service/MeasureService.kt
+++ src/main/kotlin/com/purchases/backend/service/MeasureService.kt
@@ -1,3 +1,16 @@
 package com.purchases.backend.service
 
-interface MeasureService
+import com.purchases.backend.model.Measure
+import java.util.*
+
+interface MeasureService {
+    fun getAllMeasures(): List<Measure>
+
+    fun findMeasure(name: String): Optional<Measure>
+
+    fun addMeasure(measure: Measure): Measure
+
+    fun deleteMeasure(measure: Measure)
+
+    fun deleteAllMeasures()
+}
